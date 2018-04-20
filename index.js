ea0b7c474d616bed868753883eeadeef5ede0730
Christian Williams <williamschristian1521@gmail.com>
2018-04-20 01:30:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
+
+function nestedTarget()
+{
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n)
+{
+  const nodes = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < nodes.length; i++)
+  {
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML + n)
+  }
+}
+
+function deepestChild()
+{
+  let current = document.querySelectorAll('#grand-node div')
+  return current[current.length - 1]
+}
